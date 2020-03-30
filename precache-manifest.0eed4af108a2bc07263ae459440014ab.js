1bf8759c1c6efe03b35b5554252f88ac41f0d219
ragasagar <sagarpoudel2052@gmail.com>
2020-03-30 16:30:24 +0530

    Updates

---
+++ precache-manifest.0eed4af108a2bc07263ae459440014ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602b8ce3e289be6aea1baf829b23ce49",
+    "url": "/Demon-Hub/index.html"
+  },
+  {
+    "revision": "45f1e3ea36c6d459470e",
+    "url": "/Demon-Hub/static/css/main.a2c957f7.chunk.css"
+  },
+  {
+    "revision": "8f84e21c5d2098cad329",
+    "url": "/Demon-Hub/static/js/2.1865c3e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demon-Hub/static/js/2.1865c3e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f1e3ea36c6d459470e",
+    "url": "/Demon-Hub/static/js/main.dedda988.chunk.js"
+  },
+  {
+    "revision": "dfa51c1334c72399a16d",
+    "url": "/Demon-Hub/static/js/runtime-main.cc923a74.js"
+  }
+]);
